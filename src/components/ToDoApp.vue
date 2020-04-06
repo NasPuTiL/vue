b51fcd544a77ad82ddf74ad1cf2b599ed35dc410
NasPu <nasputil@gmail.com>
2020-04-07 00:36:10 +0200

    vue test

---
+++ src/components/ToDoApp.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div>
+            <p>Wartość: {{newItem}}</p>
+            <input type ="text" placeholder="todo" v-model="newItem">
+            <button v-on:click="addItem">Add</button>  
+        </div>
+            <ToDoItem 
+                v-for="item in items" 
+                v-bind:key="item.id" 
+                v-bind:item="item"
+                @changeStgatus2="changeStatus"
+            />
+    </div>
+</template>
+
+<script>
+import ToDoItem from './ToDoItem.vue';
+
+export default {
+    name: 'ToDoAppVue',
+    components:{
+        ToDoItem
+  },
+  data(){
+    return {
+      newItem : "",
+      items: [
+        {id: 0, title: 'Zrobic zakupy', completed: false},
+        {id: 1, title: 'nagrac kurs', completed: true}
+      ],
+    }
+  },
+  methods: {
+    increase(){
+        this.counter++;
+    },
+    addItem(){
+      this.items.push({id: this.items.length, title: this.newItem,completed: false});
+      this.newItem = '';
+    },
+    changeStatus(item){
+        item.completed = !item.completed;
+    }
+  }
+
+}
+</script>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class ="item" v-bind:class="{completed: item.completed}">
+        <h2>{{item.title}}</h2>
+        <button @click="change(item)">Done</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['item'],
+    methods:{
+        change(item){
+            this.$emit('changeStgatus2', item);
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+.item {
+  border: 1px solid #cdcdcd;
+  margin: 8px;
+  padding: 10px;
+}
+.completed {
+  opacity: 0.5;
+}
+.completed h2{
+  text-decoration: line-through;
+}
+
+</style>
